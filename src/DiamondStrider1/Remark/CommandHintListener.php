b68d7c27bd6e064b56578bf43027b7a8b0e140bf
DiamondStrider1 <62265561+Swift-Strider@users.noreply.github.com>
2022-05-31 15:17:09 -0500

    Fix recursion from many listeners being registered

--- src/DiamondStrider1/Remark/CommandHintListener.php
+++ src/DiamondStrider1/Remark/CommandHintListener.php
@@ -6,70 +6,39 @@ namespace DiamondStrider1\Remark;
 
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketSendEvent;
-use pocketmine\network\mcpe\NetworkSession;
 use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
-use pocketmine\network\mcpe\protocol\Packet;
 use pocketmine\Server;
 
 final class CommandHintListener implements Listener
 {
-    /** @var Packet[] ignored to prevent recursion */
-    private array $ignored = [];
-
     public function onDataPacketSend(DataPacketSendEvent $ev): void
     {
+        $targets = $ev->getTargets();
         foreach ($ev->getPackets() as $pk) {
             if (!$pk instanceof AvailableCommandsPacket) {
                 continue;
             }
-            if (in_array($pk, $this->ignored, true)) {
-                continue;
-            }
 
-            $oldTargets = $ev->getTargets();
-            $newTargets = $oldTargets;
-            foreach ($oldTargets as $index => $target) {
-                $clonedPk = clone $pk;
-                if ($this->handleACP($clonedPk, $target)) {
-                    $this->ignored[] = $clonedPk;
-                    $target->sendDataPacket($clonedPk);
-                    unset($newTargets[$index]);
-                    $ev->cancel();
+            foreach (array_keys($pk->commandData) as $name) {
+                $command = Server::getInstance()->getCommandMap()->getCommand($name);
+                if (!$command instanceof BoundCommand) {
+                    continue;
                 }
-            }
 
-            if ($ev->isCancelled()) {
-                $this->ignored[] = $pk;
-                foreach ($newTargets as $target) {
-                    $target->sendDataPacket($pk);
+                $invisible = true;
+                foreach ($targets as $target) {
+                    $player = $target->getPlayer();
+                    if (null === $player || $command->checkVisibility($player)) {
+                        $invisible = false;
+                        continue;
+                    }
                 }
+                if ($invisible) {
+                    continue;
+                }
+
+                $pk->commandData[$name]->overloads = $command->getOverloads();
             }
-            $this->ignored = [];
         }
-    }
-
-    /**
-     * @return bool whether the packet was modified and should be resent
-     */
-    private function handleACP(AvailableCommandsPacket $pk, NetworkSession $target): bool
-    {
-        $player = $target->getPlayer();
-        if (null === $player) {
-            return false;
-        }
-
-        foreach (array_keys($pk->commandData) as $name) {
-            $command = Server::getInstance()->getCommandMap()->getCommand($name);
-            if (!$command instanceof BoundCommand) {
-                continue;
-            }
-            if (!$command->checkVisibility($player)) {
-                continue;
-            }
-
-            $pk->commandData[$name]->overloads = $command->getOverloads();
-        }
-
-        return true;
     }
 }
